fe01267632f128a9df320ef6d9320df968fe5f5f
Raphallal <raphael.laget@wanadoo.Fr>
2018-03-04 10:45:01 +0100

    RLA : Window creation

--- pck/first.py
+++ pck/first.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python
 # coding: utf-8
 
+__author__ = "raph"
 
+from tkinter import *
 
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Titre')
+    root.geometry('800x600')
+
+    root.mainloop()
